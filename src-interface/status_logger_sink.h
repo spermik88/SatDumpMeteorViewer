01fcce4d44a4407691513bec1a8949e0c0d3be68
spermik88 <serzhez79@gmail.com>
2026-01-14 03:17:07 +0400

    Update main UI screens and status bar

--- src-interface/status_logger_sink.h
+++ src-interface/status_logger_sink.h
@@ -1,16 +1,23 @@
 #pragma once
 
 #include "common/widgets/logger_sink.h"
+#include <array>
 
 namespace satdump
 {
     class StatusLoggerSink : public widgets::LoggerSinkWidget
     {
     private:
+        static constexpr size_t kLayerCount = 6;
+
         std::string str;
         std::string lvl;
         bool show_bar;
         bool show_log;
+        int layer_mode = 0;
+        std::array<bool, kLayerCount> layer_enabled{};
+
+        void draw_layer_bar();
     protected:
         void receive(slog::LogMsg log);
     public:
